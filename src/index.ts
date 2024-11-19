22e001680540fc14f576159e193207bed2d64964
Roman Pedchenko <roman.pedchenko.88@gmail.com>
2024-11-19 22:16:57 +0100

    Feat/configure (#2)
    * Create configure command
    
    * feat: configure command
    
    * feat: resolve conflicts
    
    ---------
    
    Co-authored-by: romanpedchenko <r.pedchenko@mylette.nl>

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+#!/usr/bin/env node
+
 import { Command } from 'commander';
+import { configure } from './commands/configure';
 
 const program = new Command();
+
+program.addCommand(configure);
+
+program.parse(process.argv);
